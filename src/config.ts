4332f38910539b4f539c618608e6d2acbcfad1b8
David Puerta Martín <puertadavid96182@gmail.com>
2023-01-15 04:00:52 -0500

    update website with new template

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'David Puerta Martín | Website';
+export const SITE_DESCRIPTION = 'David Puerta Martín personal website template with Project Section, Resume Section, SEO Friendly, Visual themes and Responsive Desing.';
